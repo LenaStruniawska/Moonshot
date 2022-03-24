c25382b4c02a3469c74e2aea4f95eb78b76238d7
Magdalena Struniawska <mag.sok@onet.pl>
2022-03-24 08:32:00 +0100

    Initial Commit

---
+++ MoonShotMagda/MoonShotMagdaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShotMagdaApp.swift
+//  MoonShotMagda
+//
+//  Created by Slawek on 24/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonShotMagdaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
